dabbd602f9fbb528840456b2b47c0e8ff43b917e
angus <angus@localhost.localdomain>
2017-05-15 18:04:26 +1000

    read and display raw .wav files

--- code/loadAudio.py
+++ code/loadAudio.py
@@ -1,52 +1,16 @@
-# Read in a WAV and find the freq's
-import pyaudio
-import wave
+import scipy.io.wavfile as wavfile
 import numpy as np
+import matplotlib.pyplot as plt
 
-chunk = 2048
+rate, raw_data = wavfile.read("music/rome.wav")
 
-# open up a wave
-wf = wave.open('music/1265016_Your_Style_Extended_Mix.wav', 'rb')
-swidth = wf.getsampwidth()
-RATE = wf.getframerate()
-# use a Blackman window
-window = np.blackman(chunk)
-# open stream
-p = pyaudio.PyAudio()
-stream = p.open(format=
-                p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=RATE,
-                output=True)
+windowSize = 120             # 2 minutes
+window = rate*windowSize     #how many samples we need to get a window
 
-# read some data
-data = wf.readframes(chunk)
-# play stream and find the frequency of each chunk
-while len(data) == chunk * swidth:
-    # write data out to the audio stream
-    stream.write(data)
-    # unpack the data and times by the hamming window
-    indata = np.array(wave.struct.unpack("%dh" % (len(data) / swidth), \
-                                         data)) * window
-    # Take the fft and square each value
-    fftData = abs(np.fft.rfft(indata)) ** 2
-    # find the maximum
-    which = fftData[1:].argmax() + 1
-    # use quadratic interpolation around the max
-    if which != len(fftData) - 1:
-        y0, y1, y2 = np.log(fftData[which - 1:which + 2:])
-        x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
-        # find the frequency and output it
-        thefreq = (which + x1) * RATE / chunk
-        print "The freq is %f Hz." % (thefreq)
-    else:
-        thefreq = which * RATE / chunk
-        print "The freq is %f Hz." % (thefreq)
-    # read some more data
-    data = wf.readframes(chunk)
-if data:
-    stream.write(data)
-stream.close()
-p.terminate()
-
-
+plt.plot(raw_data[0:window],'r')
+plt.ylabel('amplitude')
+plt.xlabel('sample No.')
+plt.title('raw .WAV data')
+plt.axis([0, 6000000, -60000, 60000])
+plt.show()
+print('hello world')
