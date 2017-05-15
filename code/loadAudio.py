92995dc0243e8ce3acf4e2cc9331dd8f9c525b10
angus <angus@localhost.localdomain>
2017-05-15 16:47:09 +1000

    added music

--- code/loadAudio.py
+++ code/loadAudio.py
@@ -1,10 +1,52 @@
-import scipy.io.wavfile as wavfile
-import numpy
+# Read in a WAV and find the freq's
+import pyaudio
+import wave
+import numpy as np
 
-# a = read("Toneshifterz - Land Down Under (Bootleg).wav")
-# b = numpy.array(a[1])
+chunk = 2048
 
-rate,data = wavfile.read("k1h_1277_335480_20151029_205514_30_0.wav")
-data[:5]
+# open up a wave
+wf = wave.open('music/1265016_Your_Style_Extended_Mix.wav', 'rb')
+swidth = wf.getsampwidth()
+RATE = wf.getframerate()
+# use a Blackman window
+window = np.blackman(chunk)
+# open stream
+p = pyaudio.PyAudio()
+stream = p.open(format=
+                p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=RATE,
+                output=True)
 
-print('hellooo')
+# read some data
+data = wf.readframes(chunk)
+# play stream and find the frequency of each chunk
+while len(data) == chunk * swidth:
+    # write data out to the audio stream
+    stream.write(data)
+    # unpack the data and times by the hamming window
+    indata = np.array(wave.struct.unpack("%dh" % (len(data) / swidth), \
+                                         data)) * window
+    # Take the fft and square each value
+    fftData = abs(np.fft.rfft(indata)) ** 2
+    # find the maximum
+    which = fftData[1:].argmax() + 1
+    # use quadratic interpolation around the max
+    if which != len(fftData) - 1:
+        y0, y1, y2 = np.log(fftData[which - 1:which + 2:])
+        x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
+        # find the frequency and output it
+        thefreq = (which + x1) * RATE / chunk
+        print "The freq is %f Hz." % (thefreq)
+    else:
+        thefreq = which * RATE / chunk
+        print "The freq is %f Hz." % (thefreq)
+    # read some more data
+    data = wf.readframes(chunk)
+if data:
+    stream.write(data)
+stream.close()
+p.terminate()
+
+
