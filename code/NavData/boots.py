fixed error in maths formula

code/NavData/boots.py
@@ -7,6 +7,7 @@
 import time, sys
 import ps_drone         # Import PS-Drone-API
 import pickle
+import numpy as np
 
 ## create empty lists where we store the navdata
 pitch = []
@@ -48,7 +49,7 @@ def wait_nav(drone, time_s):
     start = time.time()
     while (time.time() - start) < time_s:
         
-        if drone.NavData["altitude"][0] > 2500:
+        if drone.NavData["altitude"][0] > 2500:     # drone has reached soft altitude limit
             print "drone too high, stop and land"
             drone.stop()
             print "stop"
@@ -57,18 +58,20 @@ def wait_nav(drone, time_s):
             save_nav()
             sys.exit("exited program")
     
-        if drone.NavData["vision_detect"][0] > 0:
-            drone.stop()
-            time.sleep(2.0)
+        if drone.NavData["vision_detect"][0] > 0:   # drone sees a tag
             print "detected tag, telling drone to stop and land"
-            alpha = drone.NavData["vision_detect"][7][0]
-            beta = 180.0 - alpha
-            if beta == 180.0:
-                beta = beta + 0.1
-            drone.turnAngle(beta,0.25)
-            time.sleep(2.0)
-#            drone.moveForward(0.1)
+#            drone.stop()        # stop the drone
 #            time.sleep(2.0)
+#            alpha = drone.NavData["vision_detect"][7][0]
+            self_correct(drone)
+        
+#            beta = 180.0 - alpha
+#            if beta == 180.0:
+#                beta = beta + 0.1
+#            drone.turnAngle(beta,0.25)
+#            time.sleep(2.0)
+#            drone.moveForward(0.1)
+#            time.sleep(6.0)
 
 #            drone.stop()
 #            print "stop"
@@ -77,10 +80,78 @@ def wait_nav(drone, time_s):
 #            save_nav()
 #            sys.exit("exited program")
 
+        # wait for the next data package
         while drone.NavDataCount == NDC:  time.sleep(0.001)                       # Wait until next time-unit
         NDC = drone.NavDataCount
         append_nav(drone)
 
+    return
+
+def self_correct(drone):
+    
+    drone.stop()        # stop the drone
+    time.sleep(3.0)
+    print "drone has seen marker, drone stopped"
+    
+    # get new angle reading
+    NDC = drone.NavDataCount
+    while drone.NavDataCount == NDC:  time.sleep(0.001)                       # Wait until next time-unit
+    alpha = drone.NavData["vision_detect"][7][0]
+    
+    speed_scale = 0.05   # set the speed for the movement
+    
+#    alpha = alpha + 180.0       # because of maths!! see my notepad (in red pen) for more details
+    alpha = alpha - 90.0
+
+    move1 = speed_scale*np.cos(np.deg2rad(alpha))   # left and right movements
+    move1 = float(move1)
+    move2 = speed_scale*np.sin(np.deg2rad(alpha))   # front and back movements
+    move2 = float(move2)
+#    print "motor 1 (L/R): " + move1
+#    print "motor 2 (F/B): " + move2
+    print move1
+    print move2
+
+#    drone.land()
+#    time.sleep(2.0)
+#    sys.exit("do not attempt drone.move yet")
+
+    print "starting to move away from edge"
+    drone.move(move1, move2, 0.0, 0.0)  # back away from the edge
+    time.sleep(4.0)
+    print "finished moving away from edge"
+    
+#    drone.stop()
+#    time.sleep(2.0)
+#    if alpha > 190.0:           # drone needs to turn anti-clockwise to self-correct
+#        
+#        print "drone is turning left to self-correct"
+#        drone.turnLeft(0.2)     # try to self-correct
+#        
+#        while alpha > 190.0:    # wait til angle is close enough
+#            NDC = drone.NavDataCount
+#            while drone.NavDataCount == NDC:  time.sleep(0.001) # wait for the next data package
+#            alpha = drone.NavData["vision_detect"][7][0]
+#        
+#        print "done self-correcting for angle"
+#    
+#    elif alpha < 170.0:         # drone needs to turn clockwise to self-correct
+#        
+#        print "drone is turning right to self-correct"
+#        drone.turnRight(0.2)    # try to self correct
+#        
+#        while alpha < 170.0:    # wait til angle is close enough
+#            NDC = drone.NavDataCount
+#            while drone.NavDataCount == NDC:  time.sleep(0.001) # wait for the next data package
+#            alpha = drone.NavData["vision_detect"][7][0]
+#        
+#        print "done self-correcting for angle"
+#    
+#    # all cases of the previous if statement runs this bit of code
+#    print "moving away from the edge"
+#    drone.moveForward(0.25)
+#    time.sleep(5.0)
+#    print "done moving away from edge"
     return
 
 def save_nav():
@@ -88,3 +159,5 @@ def save_nav():
     with open('navdata-vars.pickle', 'w') as f:  # Python 3: open(..., 'wb')
         pickle.dump([pitch, roll, yaw, vx, vy, vz, nav_time, mx, my, mz, altitude_ref, detect_n, detect_dist, detect_rot], f)
     print "saved navdata"
+
+    return